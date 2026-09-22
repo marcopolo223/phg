2f1d02a174eb1400cc337c6d8a542da188e16dad
marcopolo223 <42855978+marcopolo223@users.noreply.github.com>
2026-09-22 13:57:42 +0000

    v14

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "8mb",
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.public.blob.vercel-storage.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
